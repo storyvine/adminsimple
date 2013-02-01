6c29d89e821542b53f0845c0f6e90e89049b9384
Matthew Kitt <mk@mkitt.net>
2013-02-01 16:19:14 -0700

    Small layout tweaks around media queries

--- app/controllers/adminsimple/settings_controller.rb
+++ app/controllers/adminsimple/settings_controller.rb
@@ -3,8 +3,8 @@ class Adminsimple::SettingsController < Adminsimple::ApplicationController
   #skip_authorization_check
 
   def index
-    add_crumb "Hi", '/'
-    add_crumb "blah", '/'
+    # add_crumb "Hi", '/'
+    # add_crumb "blah", '/'
   end
 
 end
