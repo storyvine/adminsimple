95887793da77677345eeefe0b6f152be09ea27f9
Jay Zeschin <jay.zeschin@modeset.com>
2013-02-06 10:18:03 -0700

    Force layouts on the overridden Devise controllers

--- app/controllers/adminsimple/devise/passwords_controller.rb
+++ app/controllers/adminsimple/devise/passwords_controller.rb
@@ -2,4 +2,6 @@ class Adminsimple::Devise::PasswordsController < Devise::PasswordsController
 
   #skip_authorization_check
 
+  layout 'adminsimple/devise'
+
 end
--- app/controllers/adminsimple/devise/sessions_controller.rb
+++ app/controllers/adminsimple/devise/sessions_controller.rb
@@ -2,4 +2,6 @@ class Adminsimple::Devise::SessionsController < Devise::SessionsController
 
   #skip_authorization_check only: [:new, :create, :destroy]
 
+  layout 'adminsimple/devise'
+
 end
