a4baa9d156f35709d4a4a4e0c5cee8fdf7d97670
Ryan Boyajian <ryan.e.boyajian@gmail.com>
2014-02-27 12:03:59 -0700

    Update template to work with rails 4 version.

--- lib/generators/adminsimple/install/templates/adminsimple.rb
+++ lib/generators/adminsimple/install/templates/adminsimple.rb
@@ -2,7 +2,7 @@ Adminsimple.setup do |config|
 
   # Set the available themes
   # dark, light, and simple are available as defaults
-  config.themes = [:dark, :light, :simple]
+  config.theme = :simple
 
   # Set the CSS overrides
   # A path to a file in your project to override styles
@@ -14,7 +14,7 @@ Adminsimple.setup do |config|
 
   # Specify the layout order
   # You can reorder the default, include additional layout modules or remove unused/unsupported ones.
-  config.layout  = [:user_nav, :main_nav, :search, :content]
+  config.layout  = [:user_nav, :main_nav, :content]
 
   # Specify the devise model
   # You can provide an alternate devise model if you're using something other than admin.
@@ -39,37 +39,101 @@ Adminsimple.setup do |config|
     c.reconfirmable = true
     c.reset_password_within = 6.hours
     c.sign_out_via = :get
+    # c.secret_key = nil
   end
 
   Navigasmic.setup do |c|
-    #c.semantic_navigation :adminsimple_main_nav do |n|
-    #  n.item n.t('adminsimple.main_nav.dashboard'), proc{ adminsimple.root_path }, icon: 'home'
-    #  n.group n.t('adminsimple.main_nav.style_guide'), icon: 'info-sign', label: 8 do
-    #    n.item n.t('adminsimple.main_nav.forms'), proc{ adminsimple.styleguide_path('forms') }, icon: 'th-list'
-    #    n.item n.t('adminsimple.main_nav.tables'), proc{ adminsimple.styleguide_path('tables') }, icon: 'th'
-    #    n.item n.t('adminsimple.main_nav.elements'), proc{ adminsimple.styleguide_path('elements') }, icon: 'briefcase'
-    #    n.item n.t('adminsimple.main_nav.messages'), proc{ adminsimple.styleguide_path('messages') }, icon: 'warning-sign'
-    #    n.item n.t('adminsimple.main_nav.icons'), proc{ adminsimple.styleguide_path('icons') }, icon: 'list-ul'
-    #  end
-    #end
-    #
-    #c.semantic_navigation :adminsimple_user_nav do |n|
-    #  n.item n.t('adminsimple.user_nav.profile'), proc{ simpleadmin.edit_registration_path }, class: 'btn', icon: 'user', link_html: {rel: 'modal'}
-    #  n.item n.t('adminsimple.user_nav.sign_out'), proc{ adminsimple.destroy_session_path(Adminsimple.configuration.devise_model) }, class: 'btn', icon: 'share-alt'
-    #end
+    c.semantic_navigation :adminsimple_main_nav do |n|
+      n.item n.t('adminsimple.main_nav.dashboard'), proc{ adminsimple.root_path }, icon: 'home'
+      n.group n.t('adminsimple.main_nav.style_guide'), icon: 'info-sign', label: 5 do
+        n.item n.t('adminsimple.main_nav.forms'), proc{ adminsimple.styleguide_path('forms') }, icon: 'th-list'
+        n.item n.t('adminsimple.main_nav.tables'), proc{ adminsimple.styleguide_path('tables') }, icon: 'th'
+        n.item n.t('adminsimple.main_nav.elements'), proc{ adminsimple.styleguide_path('elements') }, icon: 'briefcase'
+        n.item n.t('adminsimple.main_nav.messages'), proc{ adminsimple.styleguide_path('messages') }, icon: 'exclamation-triangle'
+        n.item n.t('adminsimple.main_nav.icons'), proc{ adminsimple.styleguide_path('icons') }, icon: 'list-ul'
+      end
+    end
+
+    c.semantic_navigation :adminsimple_user_nav do |n|
+      n.item n.t('adminsimple.user_nav.profile'), proc{ adminsimple.edit_registration_path }, class: 'btn', icon: 'user', link_html: {rel: 'modal'}
+      n.item n.t('adminsimple.user_nav.sign_out'), proc{ adminsimple.send(:"destroy_#{Adminsimple.configuration.devise_model}_session_path") }, class: 'btn', icon: 'share'
+    end
   end
 
   SimpleForm.setup do |c|
-    # There are more configuration options, but for brevity they're kept in a
-    # different initializer -- which you can tweak here.
-
-    c.default_wrapper = :bootstrap
     c.boolean_style = :nested
-    c.button_class = 'btn'
-    c.error_notification_tag = :div
-    c.error_notification_class = 'alert alert-error'
-    c.label_class = 'control-label'
-    c.browser_validations = false
+    c.button_class = 'btn btn-primary'
+
+    c.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: {
+      input_html: { class: 'default_class' }
+    } do |b|
+
+      b.use :html5
+      b.use :min_max
+      b.use :maxlength
+      b.use :placeholder
+
+      b.optional :pattern
+      b.optional :readonly
+
+      b.use :label_input
+      b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+
+    end
+
+    c.wrappers :bootstrap3_horizontal, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+
+      b.use :html5
+      b.use :min_max
+      b.use :maxlength
+      b.use :placeholder
+
+      b.optional :pattern
+      b.optional :readonly
+
+      b.use :label, wrap_with: { class: 'col-lg-3 col-md-3' }
+      b.wrapper :right_column, tag: :div, class: 'col-lg-9 col-md-9' do |component|
+        component.use :input
+      end
+
+      b.use :hint,  wrap_with: { tag: 'span', class: 'help-block col-lg-offset-3 col-lg-9 col-md-offset-3 col-md-9' }
+      b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error col-lg-offset-3 col-lg-9 col-md-offset-3 col-md-9' }
+    end
+
+    c.wrappers :group, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: {
+      input_html: { class: 'default-class '}
+    }  do |b|
+
+      b.use :html5
+      b.use :min_max
+      b.use :maxlength
+      b.use :placeholder
+
+      b.optional :pattern
+      b.optional :readonly
+
+      b.use :label, wrap_with: { class: 'col-lg-3 col-md-3' }
+      b.use :input, wrap_with: { class: 'input-group' }
+      b.use :hint,  wrap_with: { tag: 'span', class: 'help-block col-lg-offset-3 col-lg-9 col-md-offset-3 col-md-9' }
+      b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error col-lg-offset-3 col-lg-9 col-md-offset-3 col-md-9' }
+    end
+
+    c.wrappers :checkbox_horizontal, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+      b.wrapper tag: 'div', class: 'col-lg-offset-3 col-lg-9 col-md-offset-3 col-md-9' do |ba|
+        ba.use :label_input
+      end
+    end
+
+    c.wrappers :checkbox, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+      b.wrapper tag: 'div' do |ba|
+        ba.use :label_input
+      end
+    end
+
+    c.default_wrapper = :bootstrap3_horizontal
   end
 
 end
