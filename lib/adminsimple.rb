25da73459151171e531d52c364164e076f6c7099
jejacks0n <jejacks0n@gmail.com>
2014-03-06 15:23:53 -0700

    Update adminsimple.rb

--- lib/adminsimple.rb
+++ lib/adminsimple.rb
@@ -1,6 +1,8 @@
 require 'simple_form'
 require 'navigasmic'
 require 'crummy'
+require 'inherited_resources'
+require 'has_scope'
 
 require 'adminsimple/version'
 require 'adminsimple/configuration'
