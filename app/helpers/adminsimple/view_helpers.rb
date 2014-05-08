a00a57cd6d28ee117378c158138fda2bd517ff55
jejacks0n <jejacks0n@gmail.com>
2014-05-08 16:45:37 -0600

    adds flash messages back in?

--- app/helpers/adminsimple/view_helpers.rb
+++ app/helpers/adminsimple/view_helpers.rb
@@ -5,13 +5,16 @@ module Adminsimple::ViewHelpers
     [:alert, :notice].each do |prop|
       messages << flash_message(prop) if flash[prop].present?
     end
-    messages.html_safe
+    content_tag(:div, messages.html_safe, class: 'flash-messages')
   end
 
   def flash_message(prop = :notice)
-    close_button = content_tag(:button, '&times;'.html_safe, class: 'close', data: {dismiss: 'alert'})
-    content = content_tag(:strong, "#{prop.to_s.titleize}! ") + flash[prop]
-    content_tag(:div, "#{content}#{close_button}".html_safe, class: "alert alert-#{prop}")
+    style = case prop
+      when :notice then 'success'
+      when :alert then 'danger'
+      else 'info'
+    end
+    content_tag(:div, flash[prop].html_safe, class: "alert alert-#{style}")
   end
 
   def modal_for(label, path, options = {})
